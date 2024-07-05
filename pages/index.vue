8e7c27b9546ac2608fc1db25b1744069a19dc031
safarov-en <safarov.elkhan110@gmail.com>
2024-07-05 19:47:18 +0500

    Adding the main component

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,43 @@
 <template>
-    <Button>Click me</Button>
+    <div class="grid w-full gap-4">
+        <header class="flex items-start justify-between">
+            <div class="grow">
+                <p>Hi, welcome back!</p>
+                <h1>Dashboard</h1>
+            </div>
+            <div class="w-[120px] h-[36px] bg-neutral-200"></div>
+        </header>
+        <main class="grid gap-4">
+            <Tabs default-value="account" class="w-[400px]">
+                <TabsList>
+                    <TabsTrigger value="account">
+                        Account
+                    </TabsTrigger>
+                    <TabsTrigger value="password">
+                        Password
+                    </TabsTrigger>
+                </TabsList>
+                <TabsContent value="account">
+                    Make changes to your account here.
+                </TabsContent>
+                <TabsContent value="password">
+                    Change your password here.
+                </TabsContent>
+            </Tabs>
+            <!-- <div class="flex items-center gap-4">
+                <div v-for="(item, index) in 3" :key="index" class="w-[120px] h-[36px] bg-neutral-200"></div>
+            </div>
+            <section>
+                <div class="w-full h-[360px] bg-neutral-200"></div>
+            </section> -->
+        </main>
+        <footer>
+            <div class="flex items-center gap-4">
+                <div v-for="(item, index) in 3" :key="index" class="w-full h-[260px] bg-neutral-200"></div>
+            </div>
+        </footer>
+    </div>
 </template>
-<script>
-export default {
-    setup() {
-        
-    },
-}
+<script setup>
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 </script>
---
+++ pages/transactions.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <header>
+            <p>Hi, welcome back!</p>
+            <h1>Dashboard</h1>
+        </header>
+        <main>
+            <div>
+                Tabs: Today This week This month This year
+            </div>
+            <section></section>
+        </main>
+        <footer></footer>
+    </div>
+</template>
+<script setup>
+
+</script>
