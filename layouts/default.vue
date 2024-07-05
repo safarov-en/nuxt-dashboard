ff1aa46f07e3c66e1eed33bcfc771303de730322
safarov-en <safarov.elkhan110@gmail.com>
2024-07-05 14:00:42 +0500

    Adding a logo

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex items-start">
+        <div>
+            <Sidebar />
+        </div>
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <Button>Click me</Button>
+</template>
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
