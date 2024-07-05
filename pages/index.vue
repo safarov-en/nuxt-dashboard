f2d2f22c26139ee1e494e06d7a8eb225421ab092
safarov-en <safarov.elkhan110@gmail.com>
2024-07-05 21:08:33 +0500

    Adding a chart

--- pages/index.vue
+++ pages/index.vue
@@ -8,22 +8,17 @@
             <div class="w-[120px] h-[36px] bg-neutral-200"></div>
         </header>
         <main class="grid gap-4">
-            <Tabs default-value="Today" class="w-[400px]">
-                <TabsList>
+            <Tabs default-value="Today">
+                <TabsList class="max-w-[400px]">
                     <TabsTrigger v-for="item, index in list" :key='index' :value="item.title">
                         {{item.title}}
                     </TabsTrigger>
                 </TabsList>
                 <TabsContent v-for="item, index in list" :key="index" :value="item.title">
-                    <component :is='item.component' />
+                    <!-- <component :is='item.component' /> -->
+                     <highchart :options="options" />
                 </TabsContent>
             </Tabs>
-            <!-- <div class="flex items-center gap-4">
-                <div v-for="(item, index) in 3" :key="index" class="w-[120px] h-[36px] bg-neutral-200"></div>
-            </div>
-            <section>
-                <div class="w-full h-[360px] bg-neutral-200"></div>
-            </section> -->
         </main>
         <footer>
             <div class="flex items-center gap-4">
@@ -34,7 +29,6 @@
 </template>
 <script setup>
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-// import { resolveComponent } from 'vue'
 const list = [
     {
         title: 'Today',
@@ -53,4 +47,67 @@ const list = [
         component: resolveComponent("TabsToday")
     }
 ]
+let data = ref([
+            16.0, 18.2, 23.1, 27.9, 32.2, 36.4, 39.8, 38.4, 35.5, 29.2,
+            22.0, 17.8
+        ])
+let categories = ref({
+    'today': [],
+    'week': [],
+    'month': [],
+    'year': []
+})
+let currentCategory = ref([
+            'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',
+            'Oct', 'Nov', 'Dec'
+        ])
+const options = computed(() => ({
+    chart: {
+        type: 'line',
+        animation: {
+            enabled: false
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    title: {
+        text: ''
+    },
+    xAxis: {
+        gridLineColor: 'transparent',
+        categories: currentCategory
+    },
+    yAxis: {
+        gridLineColor: 'transparent',
+        title: {
+            text: ''
+        }
+    },
+    plotOptions: {
+        line: {
+            marker: {
+                enabled: false
+            },
+            dataLabels: {
+                enabled: false
+            },
+            enableMouseTracking: true
+        }
+    },
+    series: [{
+        name: 'line',
+        lineWidth: '4px',
+        color: {
+            linearGradient: {},
+            stops: [
+                [0, 'rgba(252, 176, 69, 1)'],
+                [0.33, 'rgba(253, 29, 29, 1)'],
+                [0.66, 'rgba(131, 58, 180, 1)'],
+                [1, 'rgba(29, 217, 83, 1)']
+            ]
+        },
+        data: data.value
+    }]
+}))
 </script>
