12f3de06526280365ed6672550974bca2d0b2bb8
safarov-en <safarov.elkhan110@gmail.com>
2024-07-05 14:32:49 +0500

    Adding a menu component

---
+++ components/Sidebar/Menu.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <header class="flex items-center gap-2 p-4 hover:scale-[101%] transition cursor-pointer">
+            <Logo />
+            <p class="font-bold">Nuxt-Finance</p>
+        </header>
+        <div class="px-4 grow">
+            <div class="grid gap-2">
+                <div v-for="(item, index) in items" :key="index" class="flex items-center gap-2 px-2 py-1 transition rounded cursor-pointer hover:bg-neutral-100">
+                    <Icon size='20' :name='item.icon' color='black' />
+                    <span>{{ item.title }}</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+const items = ref([
+    {
+        title: 'Overview',
+        path: '/overview',
+        icon: 'ri:dashboard-line'
+    },
+    {
+        title: 'Transactions',
+        path: '/transactions',
+        icon: 'grommet-icons:transaction'
+    },
+    {
+        title: 'Account',
+        path: '/account',
+        icon: 'mdi-light:account'
+    },
+    {
+        title: 'Contacts',
+        path: '/contacts',
+        icon: 'material-symbols:contacts-outline'
+    },
+    {
+        title: 'Settings',
+        path: '/settings',
+        icon: 'material-symbols:settings-outline'
+    }
+])
+</script>
--- components/Sidebar/index.vue
+++ components/Sidebar/index.vue
@@ -1,5 +1,10 @@
 <template>
-    <div>
-        <Logo />
+    <div class="relative">
+        <div class="flex lg:hidden">
+            <SidebarMenu />
+        </div>
+        <div class="hidden lg:flex">
+            <SidebarMenu />
+        </div>
     </div>
 </template>
